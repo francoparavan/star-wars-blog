6a7cbe5505b6f889faca7c5c303c59e6b6deb237
Franco Paraván <francoparavan@gmail.com>
2024-08-27 18:40:49 +0000

    guardado

--- src/js/layout.js
+++ src/js/layout.js
@@ -3,17 +3,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { Characters } from "./views/characters";
+import { Planets } from "./views/planets";
+import { Vehicles } from "./views/vehicles";
 
-//create your first component
 const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
 	const basename = process.env.BASENAME || "";
 
 	return (
@@ -23,8 +21,9 @@ const Layout = () => {
 					<Navbar />
 					<Routes>
 						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
+						<Route path="/character" element={<Characters />} />
+						<Route path="/vehicles" element={<Vehicles />} />
+						<Route path="/planets" element={<Planets />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
 					</Routes>
 					<Footer />
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,53 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			characters: [],
+			planets: [],
+			vehicles: [],
+			favorites: [],
 		},
+
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			getCharacters: () => {
+				fetch("https://swapi.tech/api/people")
+					.then((res) => res.json())
+					.then((data) => {
+						setStore({ characters: data.results });
+					})
+					.catch((err) => console.error(err));
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+			getPlanets: () => {
+				fetch("https://www.swapi.tech/api/planets")
+					.then((res) => res.json())
+					.then((data) => {
+						setStore({ planets: data.results });
+					})
+					.catch((err) => console.error(err));
 			},
-			changeColor: (index, color) => {
-				//get the store
+			getVehicles: () => {
+				fetch("https://www.swapi.tech/api/vehicles")
+					.then((res) => res.json())
+					.then((data) => {
+						setStore({ vehicles: data.results });
+					})
+					.catch((err) => console.error(err));
+			},
+
+			addFavorite: (item) => {
 				const store = getStore();
+				if (!store.favorites.includes(item)) {
+					setStore({ favorites: [...store.favorites, item] });
+				}
+			},
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
+			removeFavorite: (index) => {
+				const store = getStore();
+				setStore({
+					favorites: store.favorites.filter((_, i) => i !== index)
 				});
-
-				//reset the global store
-				setStore({ demo: demo });
 			}
-		}
-	};
+		},
+	}
 };
 
 export default getState;
